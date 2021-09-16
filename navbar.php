6987e6f0ca4f23037fd77a414c4638291d0f09a1
Harsh Jain <81317304+harsh4631@users.noreply.github.com>
2021-09-16 20:02:32 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark" style="color : #fff;">
+      <img src="img/SPARK_LOGO.png", height=70px>
+      <a class="navbar-brand" href="index.php"><b>Spark BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="navhv nav-link" href="index.php" style="color : #fff;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="navhv nav-link" href="viewallcustomers.php" style="color : #fff;"><b>User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="navhv nav-link" href="transfermoney.php" style="color : #fff;"><b>Transfer Money</b></a>
+              </li>
+              <li class="navhv nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #fff;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
